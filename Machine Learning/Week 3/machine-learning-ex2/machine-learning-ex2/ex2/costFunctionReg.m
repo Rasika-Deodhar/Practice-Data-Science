6b2b903554a4cf0881cf3d338b0655b706f80d33
Rasika-Deodhar <rasikadeodhar95@gmail.com>
2018-10-17 18:28:49 +0530

    commit

--- Machine Learning/Week 3/machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
+++ Machine Learning/Week 3/machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,9 +18,21 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+%J = (1/m) * sum(-(y) * (log(sigmoid(X*theta)))' - (1-y) * (log(1 - sigmoid(X*theta)))') + (lambda/(2*m)) * sum(theta.^2);
+%J = (1/m) * (-y)' * (log(sigmoid(X*theta))) - (1-y)' * (log(1 - sigmoid(X*theta))) + (lambda/(2*m)) * theta(2:size(theta)).^2;
+%theta = theta - [(1/m) * sum((sigmoid(X*theta) .- y)' * X)] + (lambda/m * theta);
+%theta(1) = theta(1) .- [(1/m) * sum((sigmoid(X*theta) .- y)' * X)];
+%theta(1) = theta(1) - [(1/m) * sum((sigmoid(X(1,:)' .* theta(1)).-y(1))' * X(1,:))];
+%grad = [(1/m) * (sigmoid(X*theta) - y)' * X] + (lambda/m * theta(2:size(theta)));
 
+theta_new=theta(2:size(theta));
+theta_regul=[0;theta_new];
+%
+%J = (1/m) * (-y' * log(sigmoid(X*theta) - (1-y)'  * log(1-sigmoid(X*theta))) + (lambda/(2*m))) * theta_regul' * theta_regul;
+%grad = (1/m) * ((sigmoid(X*theta)-y)' * X + lambda * theta_regul);
 
-
+J = (1/m) * sum(-(y)' * (log(sigmoid(X*theta))) - (1-y)' * (log(1 - sigmoid(X*theta)))) + (lambda/(2*m)) * sum(theta_regul.^2);
+grad = [(1/m) * sum((sigmoid(X*theta)-y)' * X)] + [(lambda/m) * theta_regul];
 
 % =============================================================
 
